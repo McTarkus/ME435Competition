e9466ce630b39b02b8074aa3111e22225af06056
McTarkus <38666790+McTarkus@users.noreply.github.com>
2018-05-01 13:53:52 -0400

    adding menu next button and some new layouts

--- IntegratedImageRec/app/src/main/java/edu/edu/rosehuman/alumbajt/integratedimagerec/MainActivity.java
+++ IntegratedImageRec/app/src/main/java/edu/edu/rosehuman/alumbajt/integratedimagerec/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper mViewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mViewFlipper = (ViewFlipper) findViewById(R.id.my_view_flipper);
     }
 
     @Override
@@ -37,13 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.getItemId() == R.id.action_next){
+            mViewFlipper.showNext();
             return true;
         }
 

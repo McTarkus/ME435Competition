679082298a8ee11b8f0a4fc55af9a0970d5f1149
McTarkus <38666790+McTarkus@users.noreply.github.com>
2018-05-01 13:05:36 -0400

    Manifest Update, copy Color Blob Detector

--- ConeFinder/app/src/main/java/edu/edu/rosehuman/alumbajt/conefinder/ConeFinderActivity.java
+++ ConeFinder/app/src/main/java/edu/edu/rosehuman/alumbajt/conefinder/ConeFinderActivity.java
@@ -1,13 +1,31 @@
 package edu.edu.rosehuman.alumbajt.conefinder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ConeFinderActivity extends AppCompatActivity {
+
+    public static final String TAG = "ConeFinder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cone_finder);
+
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)) {
+            Log.d(TAG, "Everything should be fine with using the camera.");
+        } else {
+            Log.d(TAG, "Requesting permission to use the camera.");
+            String[] CAMERA_PERMISSONS = {
+                    Manifest.permission.CAMERA
+            };
+            ActivityCompat.requestPermissions(this, CAMERA_PERMISSONS, 0);
+        }
     }
+
 }
